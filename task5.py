c6e0b00737c4410ee4b0df07617d8b2b3d3eb3b5
rsahtasheva24-hue <r.sahtasheva24@apec.edu.kz>
2025-12-19 04:05:01 +0600

    Update task5.py

--- task5.py
+++ task5.py
@@ -1,14 +1,13 @@
-nums = list(map(int, input().split()))
-target = int(input())
+def get_fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
 
-seen = set()
-result = []
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
 
-for num in nums:
-    needed = target - num
-    if needed in seen:
-        result = [needed, num]
-        break
-    seen.add(num)
-
-print(result)
+n = int(input())
+print(get_fibonacci(n))
