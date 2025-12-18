7e4d4aa72fb8a991ab1a7bccc98804a139407b1e
rsahtasheva24-hue <r.sahtasheva24@apec.edu.kz>
2025-12-19 04:03:06 +0600

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,13 +1,8 @@
-def get_fibonacci(n):
-    if n == 0:
-        return 0
-    elif n == 1
-        return 1
-    
-    a, b = 0, 1
-    for _ in range(2, n + 1)
-        a, b = b, a + b
-    return b
+def factorial(n):
+    result = 1
+    for i in range(2, n + 1):
+        result *= i
+    return result
 
-position = int(input())
-print(get_fibonacci(position))
+n = int(input())
+print(factorial(n))
